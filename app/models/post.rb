579ec3beeea742d0952c83ba53ecc4a1469a35f0
Eric Lumpkin <eblumpki@ncsu.edu>
2013-02-01 15:53:32 -0500

    Fixed bug in previous commit.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :user, :parent
   belongs_to :user
-  belongs_to :parent, class_name => 'Post'
+  belongs_to :parent, :class_name => 'Post'
   has_many :vote
 end
