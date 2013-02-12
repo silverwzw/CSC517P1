b04b354fe5b551248a108f3feb7e36b55047844a
Eric Lumpkin <eblumpki@ncsu.edu>
2013-02-12 06:39:39 -0500

    Added post functional test.

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,49 +1,49 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  setup do
-    @post = posts(:one)
-  end
 
   test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:posts)
+    get(:index)
+    assert_response(302)
   end
 
   test "should get new" do
-    get :new
-    assert_response :success
+    get(:new)
+    assert_response(200)
   end
 
   test "should create post" do
+    session[:user_id] = 2
     assert_difference('Post.count') do
-      post :create, post: { content: @post.content }
+      post(:create, :post => { :title => 'Hi', :content => 'This is a test post.', :category => 1})
     end
 
-    assert_redirected_to post_path(assigns(:post))
+    assert_not_nil Post.find_by_title("Hi")
   end
 
   test "should show post" do
-    get :show, id: @post
-    assert_response :success
+    get(:api_show, :id => 1)
+    assert_response(200)
   end
 
   test "should get edit" do
-    get :edit, id: @post
-    assert_response :success
+    session[:user_id] = 2
+    get(:edit, :id => 1)
+    assert_response(200)
   end
 
   test "should update post" do
-    put :update, id: @post, post: { content: @post.content }
-    assert_redirected_to post_path(assigns(:post))
+    session[:user_id] = 4
+    session[:content] = nil
+    put(:update, {:id => 2, :post => [:content => "Changed this one", :category => 2]})
+    assert Post.find_by_content("Changed this one")
   end
 
   test "should destroy post" do
+    session[:user_id] = 1
     assert_difference('Post.count', -1) do
-      delete :destroy, id: @post
+      assert delete(:api_delete, :id => 2)
     end
 
-    assert_redirected_to posts_path
   end
 end
