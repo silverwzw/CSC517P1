bc7bfbc23f5ac5f9345f70d68d7cd8754f10ca0d
Eric Lumpkin <eblumpki@ncsu.edu>
2013-02-12 08:01:20 -0500

    Added vote functional test.

--- test/functional/votes_controller_test.rb
+++ test/functional/votes_controller_test.rb
@@ -1,49 +1,23 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class VotesControllerTest < ActionController::TestCase
-  setup do
-    @vote = votes(:one)
+
+  test "should vote" do
+    session[:user_id] = 2
+    post :api_add, id: 2
+    assert Vote.find_by_user_id_and_post_id(2, 2)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:votes)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create vote" do
-    assert_difference('Vote.count') do
-      post :create, vote: {  }
-    end
-
-    assert_redirected_to vote_path(assigns(:vote))
-  end
-
-  test "should show vote" do
-    get :show, id: @vote
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @vote
-    assert_response :success
-  end
-
-  test "should update vote" do
-    put :update, id: @vote, vote: {  }
-    assert_redirected_to vote_path(assigns(:vote))
+  test "should not vote" do
+    session[:user_id] = 1
+    post :api_add, id: 2
+    assert_nil Vote.find_by_user_id_and_post_id(1, 2)
   end
 
   test "should destroy vote" do
+    session[:user_id] = 1
     assert_difference('Vote.count', -1) do
-      delete :destroy, id: @vote
+      delete :api_delete, id: 1
     end
-
-    assert_redirected_to votes_path
   end
 end
